44d52abb8a082f54237f67b99abeae5ea70f2c52
glados-creator <79933806+glados-creator@users.noreply.github.com>
2025-12-01 13:48:09 +0000

    cmd cypress

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@ GRADLE_OPTS="-Xmx512m" gradle :apps:product-registry-domain-service:quarkusDev
 GRADLE_OPTS="-Xmx512m" gradle :apps:product-registry-read-service:quarkusDev
 GRADLE_OPTS="-Xmx512m" gradle :apps:store-back:quarkusDev
 NODE_OPTIONS='--max-old-space-size=200' pnpm run --filter apps-store-front start
-NODE_OPTIONS='--max-old-space-size=200' pnpm run --filter apps-store-front-e2e test
+NODE_OPTIONS='--max-old-space-size=200' pnpm run --filter apps-store-front-e2e cypress:run
