3216d506922df4d4d397a45688c9c657f4e6df60
glados-creator <79933806+glados-creator@users.noreply.github.com>
2025-12-01 13:24:34 +0000

    create end 2 end test proto folder

---
+++ launch.sh
@@ -0,0 +1,4 @@
+GRADLE_OPTS="-Xmx512m" gradle :apps:product-registry-domain-service:quarkusDev
+GRADLE_OPTS="-Xmx512m" gradle :apps:product-registry-read-service:quarkusDev
+GRADLE_OPTS="-Xmx512m" gradle :apps:store-back:quarkusDev
+NODE_OPTIONS='--max-old-space-size=200' pnpm run --filter apps-store-front start
